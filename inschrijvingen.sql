372170af793393e0d6a7941585639d361da48d0f
Jasper <jasperebeli@hotmail.nl>
2019-05-08 09:54:52 +0200

    Nieuwe leerlingen

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -5,3 +5,9 @@ INSERT INTO klassen
 	('lo9e-amo1','vlt03', 2019, 'amo'), 
 	('lo9e-amo2','bkr02', 2019, 'amo'), 
     ('lo9e-amo3','rs001', 2019, 'amo');
+    
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) VALUES
+	(90000,'Pisnight','','Piet','1994-07-27','3055 JE','Rotterdam', 93.2, 1.86, 01-01-2019),
+	(90001,'Thuis','','Ben','1993-08-05','3062NM','Rotterdam', 82.0, 1.87, 01-01-2019),
+	(90002,'Bakkers','','Rudd','1993-03-26','2465 AD','Rijsaterwoude', 87.0, 1.77, 01-01-2019),
+	(90002,'Wolfe­schlegel­stein­hausen­berger­dorff ','B','Hubert','1994-07-13','2321 HJ','Leiden', 77.5, 1.85, 01-01-2019);
