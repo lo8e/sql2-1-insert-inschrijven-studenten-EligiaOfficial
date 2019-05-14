2db46443321f28e3c09c67c5b67a485841e9d801
Jasper <jasperebeli@hotmail.nl>
2019-05-14 19:56:05 +0200

    nieuwe klas code

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -10,4 +10,10 @@ INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboorted
 	(90000,'Pisnight','','Piet','1994-07-27','3055 JE','Rotterdam', 93.2, 1.86, 01-01-2019),
 	(90001,'Thuis','','Ben','1993-08-05','3062NM','Rotterdam', 82.0, 1.87, 01-01-2019),
 	(90002,'Bakkers','','Rudd','1993-03-26','2465 AD','Rijsaterwoude', 87.0, 1.77, 01-01-2019),
-	(90002,'Wolfe­schlegel­stein­hausen­berger­dorff ','B','Hubert','1994-07-13','2321 HJ','Leiden', 77.5, 1.85, 01-01-2019);
+	(90003,'Wolfe­schlegel­stein­hausen­berger­dorff ','B','Hubert','1994-07-13','2321 HJ','Leiden', 77.5, 1.85, 01-01-2019);
+    
+    INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
+    (90000, 'lo8e-amo1'),
+    (90001, 'lo8e-amo2'),
+    (90002, 'lo8e-amo3'),
+    (95003, 'lo8e-amo4');
